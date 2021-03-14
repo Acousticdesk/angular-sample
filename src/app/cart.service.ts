8216a2b9334b8cc4c3aa32fd5d1a3e779bd1f58c
Acousticdesk <acousticdesk@gmail.com>
2021-03-14 17:21:23 +0200

    3 files modified, 3 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class CartService {
   items = [];
+
+  constructor(private http: HttpClient) {}
 
   addToCart(product) {
     this.items.push(product);
@@ -15,5 +18,9 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
